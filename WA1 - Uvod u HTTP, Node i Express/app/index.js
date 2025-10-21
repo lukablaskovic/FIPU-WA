1d8ea21fd36a806a9b153d4320a00dbf9fd7e88f
Luka Blašković <59925899+lukablaskovic@users.noreply.github.com>
2025-10-21 11:17:38 +0200

    prvi commit

--- WA1 - Uvod u HTTP, Node i Express/app/index.js
+++ WA1 - Uvod u HTTP, Node i Express/app/index.js
@@ -1,26 +1,17 @@
-// REST API
 const express = require('express');
-
 const app = express();
-
 const PORT = 3000;
-const ADDRESS = 'http://localhost';
-
-// Zapamti: imamo kolekcije koje sadrže resurse
-
-app.get('/', function (req, res) {
-  // req objekt: JavaScript objekt u kojem se nalaze informacije dolaznog HTTP zahtjeva
-  // res objekt: JavaScript objekt u kojem se nalaze informacije odlaznog HTTP odgovora
-  console.log(res);
-  res.send('<p>Pozdrav iz našeg Express.js poslužitelja</p>');
-});
-
-// app.listen gre na kraju
+// predkoraci
+// git clone <repo>
+// cd u taj repo
+// npm init -y
+// npm install express
+// napraviti index.js
 
 app.listen(PORT, error => {
   if (error) {
-    console.error(`Greška prilikom pokretanja poslužitelja: ${error}`);
+    console.error('Ne mogu startati... neda mi se..');
   } else {
-    console.log(`Express.js server je pokrenut na ${ADDRESS}:${PORT}`);
+    console.log(`Express.js poslužitelj sluša na portu ${PORT}`);
   }
 });
