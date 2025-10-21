cf765fb5f725f09a1b6f251146d8a049b39cecfa
Luka Blašković <59925899+lukablaskovic@users.noreply.github.com>
2025-10-21 09:30:35 +0200

    Commitclear

--- WA1 - Uvod u HTTP, Node i Express/app/index.js
+++ WA1 - Uvod u HTTP, Node i Express/app/index.js
@@ -1,30 +1,26 @@
+// REST API
 const express = require('express');
 
 const app = express();
+
 const PORT = 3000;
+const ADDRESS = 'http://localhost';
 
-// Ruta za početnu stranicu
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+// Zapamti: imamo kolekcije koje sadrže resurse
+
+app.get('/', function (req, res) {
+  // req objekt: JavaScript objekt u kojem se nalaze informacije dolaznog HTTP zahtjeva
+  // res objekt: JavaScript objekt u kojem se nalaze informacije odlaznog HTTP odgovora
+  console.log(res);
+  res.send('<p>Pozdrav iz našeg Express.js poslužitelja</p>');
 });
 
-// Ruta za stranicu "O nama"
-app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/public/about.html');
-});
+// app.listen gre na kraju
 
-const users = [
-  { id: 1, ime: 'Ivan', prezime: 'Horvat' },
-  { id: 2, ime: 'Ana', prezime: 'Kovačić' },
-  { id: 3, ime: 'Marko', prezime: 'Marić' }
-];
-
-// Ruta za korisnike
-app.get('/users', (req, res) => {
-  res.json(users);
-});
-
-// Pokretanje poslužitelja
-app.listen(PORT, () => {
-  console.log(`Server je pokrenut na http://localhost:${PORT}`);
+app.listen(PORT, error => {
+  if (error) {
+    console.error(`Greška prilikom pokretanja poslužitelja: ${error}`);
+  } else {
+    console.log(`Express.js server je pokrenut na ${ADDRESS}:${PORT}`);
+  }
 });
