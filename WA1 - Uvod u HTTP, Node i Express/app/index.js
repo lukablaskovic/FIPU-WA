795c9049aed06cbbaa7b4e7eed0741bcf19e1791
Luka Blašković <59925899+lukablaskovic@users.noreply.github.com>
2025-10-30 10:31:42 +0100

    update

--- WA1 - Uvod u HTTP, Node i Express/app/index.js
+++ WA1 - Uvod u HTTP, Node i Express/app/index.js
@@ -5,9 +5,26 @@ const PORT = 3005;
 // instalirati nodemon:
 // npm install -g nodemon
 
+let korisnici = [
+  { id: 1, ime: 'Marko', prezime: 'Marić' },
+  { id: 2, ime: 'Ana', prezime: 'Anić' },
+  { id: 3, ime: 'Pero', prezime: 'Perić' }
+];
+
 app.get('/', (req, res) => {
-  console.log('pozvan je GET endpoint!');
-  res.send({ poruka: 'Pozdrav iz Expres.js poslužitelja' });
+  console.log('pozvan je GET root endpoint!');
+
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/about', (req, res) => {
+  console.log(__dirname);
+  res.sendFile(__dirname + '/public/about.html');
+});
+
+app.get('/korisnici', (req, res) => {
+  console.log('Pozvana je endpoint /korisnici');
+  res.json(korisnici);
 });
 
 app.listen(PORT, error => {
--- WA2 - Usmjeravanje na Express poslužitelju/app/index.js
+++ WA2 - Usmjeravanje na Express poslužitelju/app/index.js
@@ -1,17 +1,76 @@
+// 1. npm init -y
+// 2. npm install express
+// 3. u package.json promijeniti type commonjs u type module
 import express from 'express';
+let PORT = 3000;
+let app = express();
 
-const app = express();
-const PORT = 3005;
+// middleware
+app.use(express.json());
+
+let pizze = [
+  { id: 1, naziv: 'Margerita', cijena: 9 },
+  { id: 2, naziv: 'Capriciosa', cijena: 12 },
+  { id: 3, naziv: 'Vegetariana', cijena: 18 },
+  { id: 4, naziv: 'Quattro stagioni', cijena: 15 },
+  { id: 5, naziv: 'Salami', cijena: 13 }
+];
 
 app.get('/', (req, res) => {
-  console.log('pozvan je GET endpoint!');
-  res.send({ poruka: 'Pozdrav iz Expres.js poslužitelja' });
+  // osnovna ruta
+  console.log('Pozvana GET / ruta');
+  res.send('Pozdrav.');
 });
 
+app.get('/pizze', (req, res) => {
+  res.json(pizze);
+});
+// route parametar (parametar rute)
+app.get('/pizze/:naziv', (req, res) => {
+  let naziv_pizze = req.params.naziv;
+  console.log('Tražim pizzu:', naziv_pizze);
+  // -X GET http://localhost:3000/pizze/Vegetarina
+  let trazena_pizza = pizze.find(pizza => {
+    return naziv_pizze == pizza.naziv;
+  });
+
+  // find - nadi jednog u polju
+  // findIndex - nad index jednog u polju
+  // map, filter, reduce - stuči polje
+  // splice, slice - vrati podskup
+  // some, every - boolean provjere...
+  // forEach, for of, for in
+
+  res.json(trazena_pizza);
+});
+
+app.post('/pizze', (req, res) => {
+  let nova_pizza = req.body;
+  console.log('Podaci:', nova_pizza);
+  pizze.push(nova_pizza);
+  res.json(pizze);
+});
+
+app.delete('/pizze/:id', (req, res) => {
+  let brisanje_id = req.params.id;
+
+  let index_polje = pizze.findIndex(pizza => {
+    return brisanje_id == pizza.id;
+  });
+
+  pizze.splice(index_polje, 1);
+
+  console.log(pizze);
+
+  res.json(pizze);
+});
+
+// primjer poslužitelja za naručivanje pizze
 app.listen(PORT, error => {
+  // pokretanje poslužitelja
   if (error) {
-    console.error('Ne mogu startati... neda mi se..');
-  } else {
-    console.log(`Express.js poslužitelj sluša na portu ${PORT}`);
+    console.error('Došlo je do greške u pokretanju poslužitelja...');
   }
+
+  console.log(`Aplikacija sluša na portu ${PORT}`);
 });
